d35f188a5754067219d802436dc2b817b645bb6a
Max <49086429+gucci-on-fleek@users.noreply.github.com>
2019-08-15 18:30:51 -0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@ DOCKER_CLI_EXPERIMENTAL=enabled
 docker buildx create --name IoT-Gateway
 docker buildx use IoT-Gateway
 docker buildx inspect --bootstrap
-docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 -t guccionfleek/iot-gateway --push .
+docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 -t guccionfleek/iot-gateway:$(date +%Y-%m-%d) --push .
