9dd61d2bcefb206fc88be72ecde62441d037da06
Max <49086429+gucci-on-fleek@users.noreply.github.com>
2019-09-08 00:22:24 -0600

    Really really enable buildx

--- build.sh
+++ build.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 export DOCKER_CLI_EXPERIMENTAL=enabled
-jq '. + {"experimental": "enabled"}' < ~/.docker/config.json > ~/temp_config.json
-mv ~/temp_config.json ~/.docker/config.json
+echo '{"experimental": "enabled"}' > ~/.docker/config.json
 docker buildx create --use IoT-Gateway
 docker buildx inspect --bootstrap
 docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7,linux/i386 --pull -t guccionfleek/iot-gateway:$(date +%Y-%m-%d) --push .
