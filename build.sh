5f367e63e74c470f2a163af6e166589423f76950
Max <49086429+gucci-on-fleek@users.noreply.github.com>
2019-09-08 00:56:09 -0600

    Really^5 enable buildx

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
-export DOCKER_CLI_EXPERIMENTAL=enabled
+# export DOCKER_CLI_EXPERIMENTAL=enabled
 mkdir -p ~/.docker/cli-plugins
-echo '{"experimental": "enabled"}' > ~/.docker/config.json
+# echo '{"experimental": "enabled"}' > ~/.docker/config.json
 
 wget "https://github.com/docker/buildx/releases/download/v0.3.0/buildx-v0.3.0.linux-amd64"
 chmod a+x buildx-v0.3.0.linux-amd64
 mv ./buildx-v0.3.0.linux-amd64 ~/.docker/cli-plugins/docker-buildx
 
-docker buildx create --use IoT-Gateway
+docker buildx create --use
 docker buildx inspect --bootstrap
 docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7,linux/i386 --pull -t guccionfleek/iot-gateway:$(date +%Y-%m-%d) --push .
