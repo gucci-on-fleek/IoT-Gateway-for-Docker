a81f998bcf012026d81f35976002ea41485124e2
Max <49086429+gucci-on-fleek@users.noreply.github.com>
2019-10-05 15:36:41 -0600

    Update docker-buildx; fix the autobuild

--- build.sh
+++ build.sh
@@ -3,9 +3,9 @@ export DOCKER_CLI_EXPERIMENTAL=enabled
 mkdir -p ~/.docker/cli-plugins
 echo '{"experimental":true}' | sudo tee /etc/docker/daemon.json
 
-wget "https://github.com/docker/buildx/releases/download/v0.3.0/buildx-v0.3.0.linux-amd64"
-chmod a+x buildx-v0.3.0.linux-amd64
-mv ./buildx-v0.3.0.linux-amd64 ~/.docker/cli-plugins/docker-buildx
+wget "https://github.com/docker/buildx/releases/download/v0.3.1/buildx-v0.3.1.linux-amd64"
+chmod a+x buildx-v0.3.1.linux-amd64
+mv ./buildx-v0.3.1.linux-amd64 ~/.docker/cli-plugins/docker-buildx
 
 sudo docker run --privileged linuxkit/binfmt:v0.7
 
