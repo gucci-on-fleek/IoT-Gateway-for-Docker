c09728e0349f7136c7c36184d802e4f6b87554c7
Max <49086429+gucci-on-fleek@users.noreply.github.com>
2019-10-06 22:30:39 -0600

    Revert to docker/buildkit since compiling didn't speed things up

--- build.sh
+++ build.sh
@@ -7,14 +7,8 @@ wget "https://github.com/docker/buildx/releases/download/v0.3.1/buildx-v0.3.1.li
 chmod a+x buildx-v0.3.1.linux-amd64
 mv ./buildx-v0.3.1.linux-amd64 ~/.docker/cli-plugins/docker-buildx
 
-mkdir ./binfmt/
-cd ./binfmt/
-wget https://raw.githubusercontent.com/docker/binfmt/master/binfmt/Dockerfile https://raw.githubusercontent.com/docker/binfmt/master/binfmt/main.go https://raw.githubusercontent.com/docker/binfmt/master/binfmt/etc/binfmt.d/00_linuxkit.conf
-sed -i 's|COPY etc/binfmt.d/00_linuxkit.conf|COPY 00_linuxkit.conf|' Dockerfile
-docker build --pull -t binfmt .
-sudo docker run --privileged binfmt
+sudo docker run --privileged docker/binfmt:66f9012c56a8316f9244ffd7622d7c21c1f6f28d
 
-cd ..
 docker buildx create --use
 docker buildx inspect --bootstrap
 docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 --pull -t guccionfleek/iot-gateway:$(date +%Y-%m-%d) -t guccionfleek/iot-gateway:latest --push .
