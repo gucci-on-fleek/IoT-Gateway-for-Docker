e44e4ed28d021479d66d0c7be48a906957bb5286
Max <49086429+gucci-on-fleek@users.noreply.github.com>
2020-02-23 16:57:46 -0700

    Complete refactor

--- Dockerfile
+++ Dockerfile
@@ -1,65 +1,19 @@
 FROM node:12-alpine
 
 COPY ./safe-chown.c /root/safe-chown.c
+COPY ./docker-build.sh /bin/docker-build
 
-RUN apk add --no-cache --virtual build-reqs \
-    python3-dev \
-    build-base \
-    python2 \
-    libffi-dev \
-    git \
-    shadow \
-    autoconf \
-    automake \
-    nasm \
-    zlib-dev \
-    libpng-dev \
-    libjpeg-turbo-dev \
-    libtool && \
-    apk add --no-cache \
-    libcap \
-    libffi \
-    python3 \
-    curl \
-    tini \
-    zlib \
-    libjpeg-turbo \
-    libpng && \
-    cd ~ && \
-    gcc -Wall safe-chown.c && \
-    mv a.out /bin/safe-chown && \
-    chmod u+s,a-w /bin/safe-chown && \
-    git clone --depth 1 --recursive https://github.com/pagekite/PyPagekite && \
-    git clone --depth 1 --recursive https://github.com/pagekite/PySocksipyChain && \
-    cp -R ~/PyPagekite/pagekite /usr/lib/python3*/site-packages/ && \
-    cp -R ~/PySocksipyChain/sockschain /usr/lib/python3*/site-packages/ && \
-    python3 -m ensurepip && \
-    pip3 --no-cache-dir install git+https://github.com/mozilla-iot/gateway-addon-python#egg=gateway_addon && \
-    useradd --create-home --user-group --shell /bin/sh --system --uid 4545 gateway && \
-    cd /srv && \
-    git clone --depth 1 --recursive https://github.com/mozilla-iot/gateway.git && \
-    cd gateway && \
-    sed -i 's/"segfault-handler":.*//' package.json && \
-    sed -i 's/.*SegfaultHandler.*//' src/app.js && \
-    rm pagekite.py && \
-    ln -s /usr/lib/python3*/site-packages/pagekite/__main__.py /srv/gateway/pagekite.py && \
-    export CPPFLAGS="-DPNG_ARM_NEON_OPT=0" && \
-    npm config set unsafe-perm true && \
-    npm install imagemin-webpack-plugin && \
-    npm install && \
-    npm audit fix ; \
-    ./node_modules/.bin/webpack --display errors-only ; \
-    mkdir -p /home/gateway/.mozilla-iot && \
-    chown -R gateway:gateway /home/gateway/ && \
-    rm -rf /var/cache/apk/* && \
-    find / -type f -name '*.py[co]' -delete -o -type d -name __pycache__ -delete && \
-    apk del --purge build-reqs ; \
-    npm dedupe && \
-    rm -rf ./node_modules/gifsicle ./node_modules/mozjpeg ./node_modules/optipng-bin && \
-    ln -s /usr/bin/python3 /usr/bin/python && \
-    npm prune --production && \
-    npm cache clean --force && \
-    rm -rf /var/tmp/* ~/* /tmp/*
+RUN \
+    set -e ; \
+    docker-build install_packages ; \
+    docker-build build_safe_chown ; \
+    docker-build install_pagekite ; \
+    docker-build prepare_gateway_build ; \
+    docker-build install_npm_packages ; \
+    docker-build build_gateway ; \
+    docker-build create_conf_dir ; \
+    docker-build cleanup_node ; \
+    docker-build cleanup
 
 COPY ./start.sh /srv/gateway/start.sh
 USER gateway:gateway
---
+++ docker-build.sh
@@ -0,0 +1,98 @@
+#!/bin/sh
+
+   ###########################################################
+   #           WebThings Gateway (for Docker)                 #
+   # https://github.com/gucci-on-fleek/IoT-Gateway-for-Docker #
+   ############################################################
+
+set -e
+cd ~
+
+install_packages () {
+    apk add --no-cache --virtual build-reqs \
+        python3-dev \
+        build-base \
+        python2 \
+        libffi-dev \
+        git \
+        shadow \
+        autoconf \
+        automake \
+        nasm \
+        zlib-dev \
+        libpng-dev \
+        libjpeg-turbo-dev \
+        libtool
+    apk add --no-cache \
+        libcap \
+        libffi \
+        python3 \
+        curl \
+        tini \
+        zlib \
+        libjpeg-turbo \
+        libpng
+    python3 -m ensurepip
+    pip3 --no-cache-dir install git+https://github.com/mozilla-iot/gateway-addon-python#egg=gateway_addon
+}
+
+build_safe_chown () {
+    gcc -Wall safe-chown.c
+    mv a.out /bin/safe-chown
+    chmod u+s,a-w /bin/safe-chown
+}
+
+install_pagekite () {
+    git clone --depth 1 --recursive https://github.com/pagekite/PyPagekite
+    git clone --depth 1 --recursive https://github.com/pagekite/PySocksipyChain 
+    cp -R ~/PyPagekite/pagekite /usr/lib/python3*/site-packages/
+    cp -R ~/PySocksipyChain/sockschain /usr/lib/python3*/site-packages/
+}
+
+prepare_gateway_build () {
+    cd /srv/
+    git clone --depth 1 --recursive https://github.com/mozilla-iot/gateway.git
+    cd gateway
+    sed -i 's/"segfault-handler":.*//' package.json
+    sed -i 's/.*SegfaultHandler.*//' src/app.js
+    rm pagekite.py
+    ln -s /usr/lib/python3*/site-packages/pagekite/__main__.py /srv/gateway/pagekite.py 
+    npm config set unsafe-perm true
+}
+
+install_npm_packages () {
+    cd /srv/gateway/
+    npm install imagemin-webpack-plugin
+    npm install
+    npm audit fix || true
+}
+
+build_gateway () {
+    cd /srv/gateway/
+    ./node_modules/.bin/webpack --display errors-only || true
+}
+
+create_conf_dir () {
+    useradd --create-home --user-group --shell /bin/sh --system --uid 4545 gateway
+    mkdir -p /home/gateway/.mozilla-iot && \
+    chown -R gateway:gateway /home/gateway/
+}
+
+cleanup_node () {
+    cd /srv/gateway/
+    npm dedupe && \
+    rm -rf ./node_modules/gifsicle ./node_modules/mozjpeg ./node_modules/optipng-bin
+    npm prune --production
+    npm cache clean --force
+    ln -s /usr/bin/python3 /usr/bin/python
+}
+
+cleanup () {
+    rm -rf /var/cache/apk/* && \
+    find / -type f -name '*.py[co]' -delete -o -type d -name __pycache__ -delete && \
+    apk del --purge build-reqs || true
+    rm -rf /var/tmp/* ~/* /tmp/*
+}
+
+printf '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\r~~~ Running %s \n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n' $1
+$1
